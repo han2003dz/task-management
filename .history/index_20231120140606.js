2fb4e22896b2c89627bdfbd6a1f5a577d362e1f6
han2003dz <han20031605@gmail.com>
2023-11-21 00:43:43 +0700

    fix thời gian sử dụng mã otp

---
+++ .history/index_20231120140606.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const database = require("./config/database");
+database.connect();
+
+const routesVer1 = require("./api/v1/routes/index.route");
+
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+
+app.use(bodyParser.json());
+
+routesVer1(app);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
